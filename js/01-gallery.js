aca414c7179e864940bfc5065401cfa02123abfb
Aleksandr Serebro <aleksandrserebroz@gmail.com>
2021-12-16 20:19:11 +0200

    hv7-task1-2-done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+// console.log(creteImgCard(galleryItems));
+
+const imgContainer = document.querySelector('div.gallery');
+const cardMarkup = creteImgCard(galleryItems);
+
+imgContainer.insertAdjacentHTML('beforeend', cardMarkup)
+imgContainer.addEventListener('click', onImgContainerClick)
+
+function creteImgCard(galleryItems) {
+  return galleryItems.map(({preview, original}) => {
+    return `
+    <div class="gallery__item">
+      <a class="gallery__link"
+      onclick="event.preventDefault()"
+      href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="Image description"
+       />
+      </a>
+    </div>`;
+  }).join('');;
+};
+
+function onImgContainerClick(event) {
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+ 
+  // console.log(event.target.dataset.source);
+  return event.target.dataset.source;
+
+};
