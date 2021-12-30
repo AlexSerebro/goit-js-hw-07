697f61e048235040230196f28127ca2d9430910f
Aleksandr Serebro <aleksandrserebroz@gmail.com>
2021-12-30 23:52:46 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,18 +1,19 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 // console.log(galleryItems);
 // console.log(creteImgCard(galleryItems));
 
-const imgContainer = document.querySelector('div.gallery');
+const imgContainer = document.querySelector("div.gallery");
 const cardMarkup = creteImgCard(galleryItems);
 
-imgContainer.insertAdjacentHTML('beforeend', cardMarkup)
-imgContainer.addEventListener('click', onImgContainerClick)
+imgContainer.insertAdjacentHTML("beforeend", cardMarkup);
+imgContainer.addEventListener("click", onImgContainerClick);
 
 function creteImgCard(galleryItems) {
-  return galleryItems.map(({preview, original}) => {
-    return `
+  return galleryItems
+    .map(({ preview, original }) => {
+      return `
     <div class="gallery__item">
       <a class="gallery__link"
       onclick="event.preventDefault()"
@@ -25,19 +26,40 @@ function creteImgCard(galleryItems) {
        />
       </a>
     </div>`;
-  }).join('');
-};
+    })
+    .join("");
+}
 
 function onImgContainerClick(event) {
-  
-  if (!event.target.classList.contains('gallery__image')) {
+  if (!event.target.classList.contains("gallery__image")) {
     return;
-  };
-  const instance = basicLightbox.create(`
+  }
+  const instance = basicLightbox.create(
+    `
     <img src="${event.target.dataset.source}" width="800" height="600">
-`);
+`,
+    {
+      onShow: () => {
+        // console.log("~ instance");
+        window.addEventListener("keydown", onClickEscape);
+      },
+      onClose: () => window.removeEventListener("keydown", onClickEscape),
+    }
+  );
+  const onClickEscape = (event) => {
+    if (event.key === "Escape") {
+      // console.log(event.key);
+      instance.close();
+    }
+  };
   instance.show();
-};
+}
+
+// },
+// onClose: (instance) => {
+// window.removeEventListener("keydown", onKeydown);
+
+// },
 
 // const openModal = this.classList.contains('gallery');
 // console.log(openModal)
@@ -47,3 +69,11 @@ function onImgContainerClick(event) {
 //   if ()
 //   window.addEventListener('keydown', onEscCloseModal)
 // }
+
+// const onClickEscape = (event) => {
+// if (event.key === "Escape") {
+//   console.log(event.key);
+//   instance.close();
+// }
+// };
+// window.addEventListener("keydown", onClickEscape);
